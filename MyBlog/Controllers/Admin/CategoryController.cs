7e0c231c8cadf34e8380a91353e84945da6b7354
Cihan Mert PEKER <c.mertpeker@hotmail.com>
2023-11-25 02:48:08 +0300

    completed

--- MyBlog/Controllers/Admin/CategoryController.cs
+++ MyBlog/Controllers/Admin/CategoryController.cs
@@ -20,5 +20,30 @@ namespace MyBlogMVC.Controllers.Admin
             var categories = this.context.Categories.AsNoTracking().ToList();
             return View(categories);
         }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var updatedCategory = this.context.Categories.AsNoTracking().SingleOrDefault(x => x.Id == id);
+
+            return View(updatedCategory);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Category category)
+        {
+            category.SeoUrl = category.Definition;
+
+            var updatedEntity = this.context.Categories.SingleOrDefault(x => x.Id == category.Id);
+            
+            if (updatedEntity != null)
+            {
+                updatedEntity.Definition = category.Definition;
+                updatedEntity.SeoUrl = category.SeoUrl;
+                this.context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
